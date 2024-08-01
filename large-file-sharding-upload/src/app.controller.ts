087399ff7a896d0c323dec1eb706d1be396db208
puxinyu <454100270@qq.com>
2024-08-01 22:57:24 +0800

    upate

--- large-file-sharding-upload/src/app.controller.ts
+++ large-file-sharding-upload/src/app.controller.ts
@@ -1,23 +1,46 @@
-import { Body, Controller, Get, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
 import { FilesInterceptor } from '@nestjs/platform-express';
-
+import * as fs from 'fs'
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHello(); 
   }
 
   @Post('upload')
   @UseInterceptors(FilesInterceptor('files',20,{
     dest: './uploads'
   }))
-  uploadFiles(@UploadedFiles() files:Array<Express.Multer.File> ,@Body() body): string {
+  uploadFiles(@UploadedFiles() files:Array<Express.Multer.File> ,@Body() body) {
+    console.log(body)
     console.log(files)
-    return 'sdsda'
+    const fileName = body.name.match(/(.+)\-\d+$/)[1]
+    const chunkDir = 'uploads/chunks_' + fileName
+    if(!fs.existsSync(chunkDir)){
+         fs.mkdirSync(chunkDir)
+    }
+    fs.cpSync(files[0].path,chunkDir+'/'+body.name)
+    fs.rmSync(files[0].path)
+  }
+
+  @Get('merge')
+  merge(@Query('name') name:string){
+     const chunkDir = 'uploads/chunks_' + name
+     const files = fs.readdirSync(chunkDir)
+     console.log(files)
+     let startpos = 0
+     files?.map((item)=>{
+        const filePath = chunkDir + '/' + item
+         const stream = fs.createReadStream(filePath)
+         stream.pipe(fs.createWriteStream('uploads/'+ name,{
+          start:startpos
+         }))
+         startpos+=fs.statSync(filePath).size
+     })
   }
 
 }
